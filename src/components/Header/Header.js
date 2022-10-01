ddf9f0954c1dbd45087f11bdc428c234d2529eeb
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-10-01 20:22:44 +0100

    added transparent textures image as background image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from "react"
+import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
 
 export default function Header() {
   return (
     <header>
-      <h1>Keeper App</h1>
+      <h1><EmojiObjectsIcon />Keeper App</h1>
     </header>
   )
 }
